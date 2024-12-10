cdeee7ade540ca7b4369cc0873f962d38fe22835
Boazschilman <boaz.s@commbox.io>
2024-12-10 16:04:43 +0200

    Create DisplayBrandAllocation.jsx

---
+++ src/app/extensions/DisplayBrandAllocation.jsx
@@ -0,0 +1,22 @@
+import { Button, Text, Box, Flex, hubspot } from "@hubspot/ui-extensions";
+
+hubspot.extend(({ actions }) => <Extension openIframe={actions.openIframeModal} />);
+
+const Extension = ({ openIframe }) => {
+  const handleClick = () => {
+    openIframe({
+      uri: "https://hubspot-brands.onrender.com/allocation-form",
+      height: 600,
+      width: 500,
+      title: 'Brand Allocation',
+    });
+  };
+
+  return (
+    <Box>
+      <Button onClick={handleClick}>
+        Allocate Brands
+      </Button>
+    </Box>
+  );
+};
